a82f4ce10f5544d28f5a662bc7b06a4f1ae2dcb8
Kigamekun <reksa.prayoga1012@gmail.com>
2022-07-10 20:05:06 +0700

    feat (add Licese and migration)

--- resources/views/layouts/base_user.blade.php
+++ resources/views/layouts/base_user.blade.php
@@ -18,7 +18,7 @@
     @yield('content')
     <!-- Optional JavaScript; choose one of the two! -->
     <script src="https://code.jquery.com/jquery-3.6.0.js" integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
-    crossorigin="anonymous"></script>
+        crossorigin="anonymous"></script>
 
     <!-- Option 1: Bootstrap Bundle with Popper -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
